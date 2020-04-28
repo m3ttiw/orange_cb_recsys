86af22c28c6c198812104d28ebbbb8c0eca029f9
Roberto <roberto.barile98@gmail.com>
2020-04-28 17:59:36 +0200

    fix gensimWord2Vec

--- src/offline/embedding_learner/word2vec.py
+++ src/offline/embedding_learner/word2vec.py
@@ -1,13 +1,18 @@
-class GensimWord2Vec:
+from offline.content_analyzer.information_processor import InformationProcessor
+from offline.embedding_learner.embedding_learner import Word2Vec
+from offline.memory_interfaces.memory_interfaces import InformationInterface
+
+
+class GensimWord2Vec(Word2Vec):
     """"
     Class that implements the Abstract Class Word2Vec.
     Implementation of Word2Vec using the Gensim library.
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
+        super().__init__(loader, preprocessor)
 
     def start_training(self):
         """"
         Implementation of the Abstract Method start_training in the Abstract Class Word2vec.
         """
-        pass
+        print("training")
