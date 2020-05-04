991602a00f16bcaad484134d171167dfbf5e6817
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-05-04 19:07:51 +0200

    aggiunto caso di test per il run il test è vuoto: sarà implementato quando verranno completati tutti i casi di test sottostanti.

--- src/offline/run.py
+++ src/offline/run.py
@@ -3,35 +3,36 @@ import json
 from src.offline.content_analyzer.content_analyzer_main import ContentAnalyzer, FieldConfig, ContentAnalyzerConfig, \
     FieldRepresentationPipeline
 from src.offline.content_analyzer.entity_linking import BabelPyEntityLinking
+from src.offline.content_analyzer.field_content_production_technique import TfIdfTechnique
 from src.offline.content_analyzer.nlp import OpenNLP
 from src.offline.memory_interfaces.text_interface import IndexInterface
-from src.offline.movielens_example import content_analyzer_config
 from src.offline.raw_data_extractor.raw_data_manager import RawDataConfig, RawDataManager
 from src.offline.raw_data_extractor.raw_information_source import JSONFile, CSVFile, SQLDatabase
 
-runnable_istances = {
+runnable_instances = {
     "json": JSONFile,
     "csv": CSVFile,
     "sql": SQLDatabase,
     "index": IndexInterface,
     "babelpy": BabelPyEntityLinking,
     "open_nlp": OpenNLP,
+    "tf-idf": TfIdfTechnique,
 
 }
 
+
 def config_run(config_path: str = ".\config.json"):
     config_list = json.load(open(config_path))
+    representend_content_list = list()
     for content_config in config_list:
-        print(content_config['content_type'])
-        print(content_config['raw_source_path'])
-        print(content_config['id_field_name'])
 
         # fase 1
         for field_dict in content_config['fields']:
             if field_dict['memory_interface'] != "None":
-                config_dict = {field_dict['field_name']: runnable_istances[field_dict['memory_interface']]('./test-index-plot')}
+                config_dict = {
+                    field_dict['field_name']: runnable_instances[field_dict['memory_interface']]('./test-index-plot')}
                 raw_data_config = RawDataConfig(
-                    runnable_istances[content_config['source_type']](content_config['raw_source_path']),
+                    runnable_instances[content_config['source_type']](content_config['raw_source_path']),
                     content_config['id_field_name'], config_dict)
                 RawDataManager(raw_data_config).fit()
 
@@ -39,26 +40,20 @@ def config_run(config_path: str = ".\config.json"):
         for field_dict in content_config['fields']:
             field_config = FieldConfig()
             content_analyzer_config = ContentAnalyzerConfig(
-                runnable_istances[content_config['source_type']](content_config['raw_source_path']),
+                runnable_instances[content_config['source_type']](content_config['raw_source_path']),
                 content_config['id_field_name'])
             for pipeline_dict in field_dict['pipeline_list']:
                 preprocessing_list = list()
                 for preprocessing in pipeline_dict['preprocesing_list']:
                     class_name = preprocessing.pop('class')
-                    preprocessing_list.append(runnable_istances['class_name'](**preprocessing))
+                    preprocessing_list.append(runnable_instances['class_name'](**preprocessing))
                 class_name = pipeline_dict['field_content_production'].pop('class')
-                field_config.add_pipeline(FieldRepresentationPipeline(runnable_istances[class_name](**pipeline_dict)),
-                                          preprocessing_list)
+                field_config.add_pipeline(FieldRepresentationPipeline(runnable_instances[class_name](**pipeline_dict),
+                                                                      preprocessing_list))
 
-            content_analyzer = ContentAnalyzer(content_analyzer_config)
-            represented_contents = content_analyzer.fit()
+        content_analyzer = ContentAnalyzer(content_analyzer_config)
+        representend_content_list.append(content_analyzer.fit())
 
-
-
-
-
-
-
-
+    return representend_content_list
 
 config_run()
---
+++ test/offline/test_run.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_config_run(self):
+        self.skipTest("test in the submodules.")
