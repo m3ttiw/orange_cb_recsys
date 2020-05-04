2343b935f46535e40b12cfc7be772ea68324d560
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-05-04 20:35:33 +0200

    migliorato il test con la funzione test_dict_keys.

--- test/offline/test_run.py
+++ test/offline/test_run.py
@@ -1,8 +1,18 @@
 import json
+from typing import Dict, List
 from unittest import TestCase
 
 
 class Test(TestCase):
+
+    def test_dict_key(self,
+                      test_dict: Dict,
+                      keys: List[str],
+                      msg: str = "You have to put the {} in the {}",
+                      context: str = "dictionary"):
+        for key in keys:
+            self.assertIn(key, test_dict.keys(), msg.format(key, context))
+
     def test_config(self):
         config_list = json.load(open("src\offline\config.json"))
         with self.assertRaises(FileNotFoundError) as cm:
@@ -11,32 +21,19 @@ class Test(TestCase):
         for content_config in config_list:
             self.assertEqual(type(content_config), type(dict()),
                              "the config must contain a list of dict")
-            self.assertIn("content_type", content_config.keys(),
-                          "You have to put the 'content_type' in the content config")
-            self.assertIn("raw_source_path", content_config.keys(),
-                          "You have to put the 'raw_source_path' in the content config")
-            self.assertIn("source_type", content_config.keys(),
-                          "You have to put the 'source_type' in the content config")
-            self.assertIn("id_field_name", content_config.keys(),
-                          "You have to put the 'id_field_name' in the content config")
-            self.assertIn("fields", content_config.keys(),
-                          "You have to put the 'fields' in the content config")
+            self.test_dict_key(content_config, ["content_type", "raw_source_path", "source_type", "id_field_name",
+                                                "fields"], context="content config")
             for field in content_config['fields']:
                 if field is not None:
-                    self.assertIn("field_name", field.keys(),
-                                  "You have to put the 'field_name' in the field config")
-                    self.assertIn("memory_interface", field.keys(),
-                                  "You have to put the 'memory_interface' in the field config")
-                    self.assertIn("pipeline_list", field.keys(),
-                                  "You have to put the 'pipeline_list' in the field config")
+                    self.test_dict_key(field,
+                                       ["field_name", "memory_interface", "pipeline_list"], context="field config")
                     for pipeline in field["pipeline_list"]:
                         if pipeline is not None:
-                            self.assertIn("field_content_production", pipeline.keys(),
-                                          "You have to put the 'field_content_production' in the pipeline config")
-                            self.assertIn("class", pipeline["field_content_production"].keys(),
-                                          "You have to put the 'class' in the field_content_production config")
-                            self.assertIn("preprocessing_list", pipeline.keys(),
-                                          "You have to put the 'preprocessing_list' in the pipeline config")
+                            self.test_dict_key(field,
+                                               ["field_content_production", "preprocessing_list"],
+                                               context="pipeline config")
+                            self.test_dict_key(field["field_content_production"],
+                                               ["class"], context="field_content_production config")
                             for preprocessing in pipeline["preprocessing_list"]:
                                 pass
 
