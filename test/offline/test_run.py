992d89a0495e832cab5dfaffc5b70d13d8d79bb0
Mattia Patruno <mattiapatruno98@outlook.it>
2020-05-08 16:22:24 +0200

    fixato test_config

--- test/offline/test_run.py
+++ test/offline/test_run.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 
 
 class Test(TestCase):
-
+    """
     def test_dict_key(self,
                       test_dict: Dict,
                       keys: List[str],
@@ -14,32 +14,50 @@ class Test(TestCase):
                       context: str = "dictionary"):
         for key in keys:
             self.assertIn(key, test_dict.keys(), msg.format(key, context))
+    """
 
     def test_config(self):
-        self.skipTest("FIX TEST")
         # test only if the key in the config.json are valid
         config_list = json.load(open("src\offline\config.json"))
+        msg: str = "You have to put the {} in the {}"
         with self.assertRaises(FileNotFoundError):
             self.assert_(True, "Try to use double back_slashes '\\' instead of a single slash")
         self.assertEqual(type(config_list), type(list()), "the config must contain a list of dict")
         for content_config in config_list:
             self.assertEqual(type(content_config), type(dict()),
                              "the config must contain a list of dict")
+            """
             self.test_dict_key(content_config, ["content_type", "raw_source_path", "source_type", "id_field_name",
-                                                "fields"], context="content config")
+                                                "fields"], context=)
+            """
+            for key in ["content_type", "raw_source_path", "source_type", "id_field_name", "fields"]:
+                self.assertIn(key, content_config.keys(), msg.format(key, "content config"))
             for field in content_config['fields']:
                 if field is not None:
+                    """
                     self.test_dict_key(field,
                                        ["field_name", "memory_interface", "pipeline_list"], context="field config")
+                    """
+                    for key in ["field_name", "memory_interface", "pipeline_list"]:
+                        self.assertIn(key, field.keys(), msg.format(key, "field config"))
                     for pipeline in field["pipeline_list"]:
                         if pipeline is not None:
+                            """
                             self.test_dict_key(field,
                                                ["field_content_production", "preprocessing_list"],
                                                context="pipeline config")
                             self.test_dict_key(field["field_content_production"],
                                                ["class"], context="field_content_production config")
+                            """
+                            for key in ["field_content_production", "preprocessing_list"]:
+                                self.assertIn(key, field.keys(), msg.format(key, "pipeline config"))
+                            self.assertIn("class", field["field_content_production"].keys(),
+                                          msg.format(key, "field_content_production config"))
                             for preprocessing in pipeline["preprocessing_list"]:
+                                """
                                 self.test_dict_key(preprocessing, ["class"], context="preprocessing config")
+                                """
+                                self.assertIn("class", preprocessing.keys(),msg.format(key, "preprocessing config"))
 
     def test_run(self):
         self.skipTest("test in the submodules.")
