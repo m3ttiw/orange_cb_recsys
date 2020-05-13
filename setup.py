2206500a1a1bfe33e806bbffa5955a25048afcd6
Mattia Patruno <mattiapatruno98@outlook.it>
2020-05-13 21:03:16 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='Framework_CBRS_py',
       version='1.0',
       description='Python Framework for Content-Based Recommeder Systems',
       url='https://github.com/m3ttiw/Framework_CBRS_py',
-      packages=['src', 'offline', 'online', 'test', ],
-      package_dir={'src': 'src', 'offline': 'src/offline', 'online': 'src/online', 'test': 'test', },
+      packages=['src', 'test', ],
+      package_dir={'src': 'src', 'test': 'test', },
       )
 
 """
