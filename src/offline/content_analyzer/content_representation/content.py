20ded651d486dcaa7bae9d939d92a8347e3b0d8f
itsfrank98 <frybene@libero.it>
2020-05-15 11:36:58 +0200

    Content

--- src/offline/content_analyzer/content_representation/content.py
+++ src/offline/content_analyzer/content_representation/content.py
@@ -1,46 +1,37 @@
 from typing import List
-import pickle
-import os
 from src.offline.content_analyzer.content_representation.content_field import ContentField
 
 
 class Content:
     """
-    Class that represent a content,
-    a content can be an item, a user or a rating
+    Class that represents a content. A content can be an item, a user or a rating
     A content is identified by a string id and is composed of different fields
     Args:
         content_id (str): identifier
         field_list (list[ContentField]): list of the fields instances of a content
     """
-
     def __init__(self, content_id: str,
                  field_list: List[ContentField] = None):
         if field_list is None:
-            field_list = []  # list o dict
+            field_list = []         # list o dict
         self.__content_id: str = content_id
         self.__field_list: List[ContentField] = field_list
-
-    def load(self, path):
-        self = pickle.loads(path)
 
     def append(self, field: ContentField):
         self.__field_list.append(field)
 
     def remove(self, field_name: str):
         """
-        remove the field with field_name in the fields list
+        Remove the field named field_name from the field list
         Args:
             field_name (str): the name of the field to remove
         """
         self.__field_list.pop(self.__field_list.index(ContentField(field_name)))
 
-    def serialize(self, output_directory: str):
+    def serialize(self):
         """
-        Serialize a content instance
+        Serialize an item
         """
-        file = open(output_directory + '/' + self.__content_id + '.bin', 'wb')
-        pickle.dump(self, file)
 
     def __str__(self):
         content_string = "Content:" + self.__content_id
@@ -56,10 +47,9 @@ class RepresentedContents:
     Class that collects the Contents instance created,
     the whole collection can be serialized.
     Args:
-        content_list (list<Item>): list of content's instances
         length: number of contents
+        content_list (list<Content>): list of content's instances
     """
-
     def __init__(self, length: int = 0,
                  content_list: List[Content] = None):
         if content_list is None:
