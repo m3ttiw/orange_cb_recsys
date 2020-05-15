8e5db01153d1cde9513908d278a4001c0bc82c4d
itsfrank98 <frybene@libero.it>
2020-05-15 11:43:08 +0200

    id merger

--- src/offline/utils/id_merger.py
+++ src/offline/utils/id_merger.py
@@ -1,4 +1,15 @@
 def id_merger(id_list):
+    """
+    This method ise used to compact a list of ids into a unique string. This can be useful when
+    there is content whose id is composed by values coming from more than one field.
+
+    Args:
+        id_list (List<str>): List containing one or more ids
+
+    Returns:
+        id_merged (str): String in which the values contained in the list given in input are
+            merged
+    """
     if type(id_list) == str or type(id_list) == int:
         return str(id_list)
     elif type(id_list) == list:
