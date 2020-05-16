d6723d71fb87aeaeef56d5a9e329b3d42219218b
Mattia Patruno <mattiapatruno98@outlook.it>
2020-05-16 12:52:58 +0200

    aggiunti test per content.py

--- test/offline/content_analyzer/content_representation/test_content.py
+++ test/offline/content_analyzer/content_representation/test_content.py
@@ -1,15 +1,28 @@
 from unittest import TestCase
 
+from offline.content_analyzer.content_representation.content import Content
+from offline.content_analyzer.content_representation.content_field import FeaturesBagField, ContentField
+
 
 class TestContent(TestCase):
-    def test_load(self):
-        self.fail()
+    def test_load_serialize(self):
+        content_field_repr = FeaturesBagField("test")
+        content_field_repr.append_feature("test_key", "test_value")
+        content_field = ContentField("test_field", "0000")
+        content_field.append(content_field_repr)
+        content = Content("001")
+        content_not_serialized = content
+        content.append(content_field)
+        content.serialize("test_dir")
+        content.load("test_dir")
+        self.assertEqual(content_not_serialized, content)
 
-    def test_append(self):
-        self.fail()
+    def test_append_remove(self):
+        content_field_repr = FeaturesBagField("test")
+        content_field_repr.append_feature("test_key", "test_value")
+        content_field = ContentField("test_field", "0000")
+        content_field.append(content_field_repr)
+        content = Content("001")
+        content.append(content_field)
+        self.assertEqual(content.remove("test_field"), content_field)
 
-    def test_remove(self):
-        self.fail()
-
-    def test_serialize(self):
-        self.fail()
