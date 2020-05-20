2587a001df371c6d12a11d41094f735c1be2e694
Mattia Patruno <mattiapatruno98@outlook.it>
2020-05-20 18:58:24 +0200

    Update embedding_learner.py con source loader

--- src/content_analyzer/embedding_learner/doc2vec.py
+++ src/content_analyzer/embedding_learner/doc2vec.py
@@ -1,26 +1,48 @@
-from src.content_analyzer.embedding_learner.embedding_learner import Doc2Vec
+from src.content_analyzer.embedding_learner import embedding_learner
 from src.content_analyzer.information_processor.information_processor import InformationProcessor
-from src.content_analyzer.memory_interfaces.memory_interfaces import InformationInterface
+from src.content_analyzer.raw_information_source import RawInformationSource
+from gensim.test.utils import get_tmpfile
+from gensim.test.utils import common_texts
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
-class GensimDoc2Vec(Doc2Vec):
+class GensimDoc2Vec(embedding_learner.Doc2Vec):
     """"
     Class that implements the Abstract Class Word2Vec.
     Implementation of Word2Vec using the Gensim library.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    is_first_instance = True
+
+    def __init__(self, source: RawInformationSource,
+                 preprocessor: InformationProcessor,
+                 **kwargs):
+        super().__init__(source, preprocessor)
+
+        if "model_path" in kwargs.keys():
+            self.__fname = get_tmpfile(str(kwargs["model_path"]))
+        else:
+            self.__fname = get_tmpfile("doc2vec_model")
+
+        if GensimDoc2Vec.is_first_instance:
+            documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(common_texts)]
+            self.__model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
+        elif "use_pre_trained_model" in kwargs.keys():
+            self.__model = Doc2Vec.load(self.__fname)  # you can continue training with the loaded model!
+
+        self.__model.save(self.__fname)
+        GensimDoc2Vec.is_first_instance = False
 
     def __str__(self):
         return "GensimDoc2Vec"
 
     def __repr__(self):
         return "< GensimDoc2Vec :" + \
-                "loader = " + str(self.__loader) + \
-                "preprocessor = " + str(self.__preprocessor) + " >"
+               "loader = " + str(self.__source) + \
+               "preprocessor = " + str(self.__preprocessor) + " >"
 
     def start_learning(self):
         """"
-        Implementation of the Abstract Method start_training in the Abstract Class Word2vec.
+        Implementation of the Abstract Method start_training in the Abstract Class Doc2vec.
         """
+
         print("learning")
--- src/content_analyzer/embedding_learner/embedding_learner.py
+++ src/content_analyzer/embedding_learner/embedding_learner.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 from src.content_analyzer.information_processor.information_processor import InformationProcessor
-from src.content_analyzer.memory_interfaces.memory_interfaces import InformationInterface
+from src.content_analyzer.raw_information_source import RawInformationSource
 
 
 class EmbeddingLearner(ABC):
@@ -12,8 +12,8 @@ class EmbeddingLearner(ABC):
         loader (InformationLoader): Instance of the class InformationLoader.
         preprocessor (InformationProcessor): Instance of the class InformationProcessor.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        self.__loader: InformationInterface = loader
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        self.__source: RawInformationSource = source
         self.__preprocessor: InformationProcessor = preprocessor
 
     @abstractmethod
@@ -25,8 +25,8 @@ class Word2Vec(EmbeddingLearner):
     """"
     Abstract Class for the different kinds of Word2Vec.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        super().__init__(source, preprocessor)
 
     @abstractmethod
     def start_learning(self):
@@ -40,8 +40,8 @@ class Doc2Vec(EmbeddingLearner):
     """"
     Abstract Class for the different kinds of Doc2Vec.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        super().__init__(source, preprocessor)
 
     @abstractmethod
     def start_learning(self):
@@ -55,8 +55,8 @@ class LatentSemanticAnalysis(EmbeddingLearner):
     """"
     Abstract Class for the different kinds of Latent Semantic Analysis.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        super().__init__(source, preprocessor)
 
     @abstractmethod
     def start_learning(self):
@@ -70,8 +70,8 @@ class RandomIndexing(EmbeddingLearner):
     """"
     Abstract Class for the different kinds of Random Indexing.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        super().__init__(source, preprocessor)
 
     @abstractmethod
     def start_learning(self):
@@ -85,8 +85,8 @@ class ExplicitSemanticAnalysis(EmbeddingLearner):
     """"
     Abstract Class for the different kinds of Explicit Semantic Analysis.
     """
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
-        super().__init__(loader, preprocessor)
+    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+        super().__init__(source, preprocessor)
 
     @abstractmethod
     def start_learning(self):
