493384a2a353babdf166152d1463b8dea2172903
Mattia Patruno <mattiapatruno98@outlook.it>
2020-05-21 11:55:59 +0200

    fix test

--- test/content_analyzer/embedding_learner/test_doc2vec.py
+++ test/content_analyzer/embedding_learner/test_doc2vec.py
@@ -20,11 +20,13 @@ class TestGensimDoc2Vec(TestCase):
                       0.24684472, 0.552818, 0.10604687, -0.47952488, 0.18031257, -0.14340155, -0.370736, 0.3842013,
                       -0.34952304, -0.70719564, -0.00113534, 0.09173155, 0.05633677]]
         try:
-            test_results = GensimDoc2Vec(source=JSONFile(file_path="datasets/d2v_test_data.json"),
-                                         preprocessor=NLTK(), field_name="doc_field").start_learning()
+            path = "datasets/d2v_test_data.json"
+            open(path)
         except FileNotFoundError:
-            test_results = GensimDoc2Vec(source=JSONFile(file_path="../../../datasets/d2v_test_data.json"),
-                                         preprocessor=NLTK(), field_name="doc_field").start_learning()
+            path = "../../../datasets/d2v_test_data.json"
+
+        test_results = GensimDoc2Vec(source=JSONFile(file_path=path),
+                                     preprocessor=NLTK(), field_name="doc_field").start_learning()
 
         for i, res in enumerate(test_results):
             self.assertEqual(test_list[i], res, "Fail in Doc {} - Vector = {}".format(str(i), res))
