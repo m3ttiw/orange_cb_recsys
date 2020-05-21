7c2177d9f414baae0610c49c0a3306382c307444
Carlo Parisi <carlo.parisi01234@gmail.com>
2020-05-21 13:13:51 +0200

    Rimosso tutti i parametri di FastText non utilizzati

--- src/content_analyzer/embedding_learner/fasttext.py
+++ src/content_analyzer/embedding_learner/fasttext.py
@@ -34,72 +34,6 @@ class GensimFastText(embedding_learner.FastText):
         else:
             self.__min_count = 1
 
-        if "workers" in kwargs.keys():
-            self.__workers = kwargs["workers"]
-
-        if "alpha" in kwargs.keys():
-            self.__alpha = kwargs["alpha"]
-
-        if "min_alpha" in kwargs.keys():
-            self.__min_alpha = kwargs["min_alpha"]
-
-        if "sg" in kwargs.keys():
-            self.__sg = kwargs["sg"]
-
-        if "hs" in kwargs.keys():
-            self.__hs = kwargs["hs"]
-
-        if "seed" in kwargs.keys():
-            self.__seed = kwargs["seed"]
-
-        if "max_vocab_size" in kwargs.keys():
-            self.__max_vocab_size = kwargs["max_vocab_size"]
-
-        if "sample" in kwargs.keys():
-            self.__sample = kwargs["sample"]
-
-        if "negative" in kwargs.keys():
-            self.__negative = kwargs["negative"]
-
-        if "ns_exponent" in kwargs.keys():
-            self.__ns_exponent = kwargs["ns_exponent"]
-
-        if "cbow_mean" in kwargs.keys():
-            self.__cbow_mean = kwargs["cbow_mean"]
-
-        if "hashfxn" in kwargs.keys():
-            self.__hashfxn = kwargs["hashfxn"]
-
-        if "iter" in kwargs.keys():
-            self.__iter = kwargs["iter"]
-
-        if "trim_rule" in kwargs.keys():
-            self.__trim_rule = kwargs["trim_rule"]
-
-        if "batch_words" in kwargs.keys():
-            self.__batch_words  = kwargs["batch_words"]
-
-        if "min_n" in kwargs.keys():
-            self.__min_n = kwargs["min_n"]
-
-        if "max_n" in kwargs.keys():
-            self.__max_n = kwargs["max_n"]
-
-        if "word_ngrams" in kwargs.keys():
-            self.__word_ngrams = kwargs["word_ngrams"]
-
-        if "bucket" in kwargs.keys():
-            self.__bucket = kwargs["bucket"]
-
-        if "callbacks" in kwargs.keys():
-            self.__callbacks = kwargs["callbacks"]
-
-        if "compatible_hash" in kwargs.keys():
-            self.__compatible_hash = kwargs["compatible_hash"]
-
-        if "sorted_vocab" in kwargs.keys():
-            self.__sorted_vocab = kwargs["sorted_vocab"]
-
         if "ephocs" in kwargs.keys():
             self.__epochs = kwargs["ephocs"]
         else:
@@ -123,7 +57,6 @@ class GensimFastText(embedding_learner.FastText):
         """
         model = FastText(size=self.__size, window=self.__window, min_count=self.__min_count)
         total_examples = model.corpus_count
-        model = self.__get_model()
         model.build_vocab(senteces=GensimFastText(self.__source, self.__preprocessor, self.__field_name))
         model.train(sentences=GensimFastText(self.__source, self.__preprocessor, self.__field_name),
                     total_examples=total_examples, epochs=self.__epochs)
