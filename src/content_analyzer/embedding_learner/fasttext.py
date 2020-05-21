62655fda0d4f16ee1719b7cfd0001539426998f3
Carlo Parisi <carlo.parisi01234@gmail.com>
2020-05-21 02:34:00 +0200

    definito tutti i possibili kwargs per il FastText

--- src/content_analyzer/embedding_learner/fasttext.py
+++ src/content_analyzer/embedding_learner/fasttext.py
@@ -1,16 +1,103 @@
-from src.content_analyzer.embedding_learner.embedding_learner import FastText
-from src.content_analyzer.information_processor.information_processor import InformationProcessor
+from src.content_analyzer.embedding_learner import embedding_learner
+from src.content_analyzer.information_processor.information_processor import NLP
 from src.content_analyzer.memory_interfaces.memory_interfaces import InformationInterface
+from gensim.models.fasttext import FastText
+from gensim.utils import tokenize
+from gensim.test.utils import datapath
+from gensim import utils
 
 
-class GensimFastText(FastText):
+class GensimFastText(embedding_learner.FastText):
     """"
     Class that implements the Abstract Class FastText.
     Implementation of FastText using the Gensim library.
     """
 
-    def __init__(self, loader: InformationInterface, preprocessor: InformationProcessor):
+    def __init__(self, loader: InformationInterface, preprocessor: NLP, field_name: str, **kwargs):
+        preprocessor.set_is_tokenized(True)
         super().__init__(loader, preprocessor)
+        self.__field_name = field_name
+
+        if "size" in kwargs.keys():
+            self.__size = kwargs["size"]
+        else:
+            self.__size = 4
+
+        if "window" in kwargs.keys():
+            self.__window = kwargs["window"]
+        else:
+            self.__window = 3
+
+        if "min_count" in kwargs.keys():
+            self.__min_count = kwargs["min_count"]
+        else:
+            self.__min_count = 1
+
+        if "workers" in kwargs.keys():
+            self.__workers = kwargs["workers"]
+
+        if "alpha" in kwargs.keys():
+            self.__alpha = kwargs["alpha"]
+
+        if "min_alpha" in kwargs.keys():
+            self.__min_alpha = kwargs["min_alpha"]
+
+        if "sg" in kwargs.keys():
+            self.__sg = kwargs["sg"]
+
+        if "hs" in kwargs.keys():
+            self.__hs = kwargs["hs"]
+
+        if "seed" in kwargs.keys():
+            self.__seed = kwargs["seed"]
+
+        if "max_vocab_size" in kwargs.keys():
+            self.__max_vocab_size = kwargs["max_vocab_size"]
+
+        if "sample" in kwargs.keys():
+            self.__sample = kwargs["sample"]
+
+        if "negative" in kwargs.keys():
+            self.__negative = kwargs["negative"]
+
+        if "ns_exponent" in kwargs.keys():
+            self.__ns_exponent = kwargs["ns_exponent"]
+
+        if "cbow_mean" in kwargs.keys():
+            self.__cbow_mean = kwargs["cbow_mean"]
+
+        if "hashfxn" in kwargs.keys():
+            self.__hashfxn = kwargs["hashfxn"]
+
+        if "iter" in kwargs.keys():
+            self.__iter = kwargs["iter"]
+
+        if "trim_rule" in kwargs.keys():
+            self.__trim_rule = kwargs["trim_rule"]
+
+        if "batch_words" in kwargs.keys():
+            self.__batch_words  = kwargs["batch_words"]
+
+        if "min_n" in kwargs.keys():
+            self.__min_n = kwargs["min_n"]
+
+        if "max_n" in kwargs.keys():
+            self.__max_n = kwargs["max_n"]
+
+        if "word_ngrams" in kwargs.keys():
+            self.__word_ngrams = kwargs["word_ngrams"]
+
+        if "bucket" in kwargs.keys():
+            self.__bucket = kwargs["bucket"]
+
+        if "callbacks" in kwargs.keys():
+            self.__callbacks = kwargs["callbacks"]
+
+        if "compatible_hash" in kwargs.keys():
+            self.__compatible_hash = kwargs["compatible_hash"]
+
+        if "sorted_vocab" in kwargs.keys():
+            self.__sorted_vocab = kwargs["sorted_vocab"]
 
     def __str__(self):
         return "FastText"
