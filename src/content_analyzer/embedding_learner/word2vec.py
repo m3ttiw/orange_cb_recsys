507ac4be21c1699caf8e0e7714fd660079793d36
Carlo Parisi <carlo.parisi01234@gmail.com>
2020-05-21 18:20:39 +0200

    Aggiornato la struttura di GensimWord2Vec

--- src/content_analyzer/embedding_learner/word2vec.py
+++ src/content_analyzer/embedding_learner/word2vec.py
@@ -1,14 +1,21 @@
-from src.content_analyzer.embedding_learner.embedding_learner import Word2Vec
+from typing import List
+
+from src.content_analyzer.embedding_learner import embedding_learner
 from src.content_analyzer.information_processor.information_processor import InformationProcessor
 from src.content_analyzer.raw_information_source import RawInformationSource
 
 
-class GensimWord2Vec(Word2Vec):
+class GensimWord2Vec(embedding_learner.EmbeddingLearner):
     """"
     Class that implements the Abstract Class Word2Vec.
     Implementation of Word2Vec using the Gensim library.
     """
-    def __init__(self, source: RawInformationSource, preprocessor: InformationProcessor):
+
+    def __init__(self, source: RawInformationSource,
+                 preprocessor: InformationProcessor,
+                 field_list=List[str],
+                 **kwargs):
+        self.__field_list = field_list
         super().__init__(source, preprocessor)
 
     def __str__(self):
@@ -16,10 +23,10 @@ class GensimWord2Vec(Word2Vec):
 
     def __repr__(self):
         return "< GensimWord2Vec :" + \
-                "source = " + str(self.__source) + \
-                "preprocessor = " + str(self.__preprocessor) + " >"
+               "source = " + str(self.__source) + \
+               "preprocessor = " + str(self.__preprocessor) + " >"
 
-    def start_learning(self):
+    def fit(self):
         """"
         Implementation of the Abstract Method start_training in the Abstract Class Word2vec.
         """
