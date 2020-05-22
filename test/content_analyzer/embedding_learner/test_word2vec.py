3872b3ff85e7ef2ef58d2cf43de9de835c06c191
Carlo Parisi <carlo.parisi01234@gmail.com>
2020-05-22 15:01:01 +0200

    fix bug

--- test/content_analyzer/embedding_learner/test_word2vec.py
+++ test/content_analyzer/embedding_learner/test_word2vec.py
@@ -8,11 +8,12 @@ import os
 
 class TestGensimWord2Vec(TestCase):
     def test_fit(self):
+        field_list = ['Title', 'Year', 'Genre']
         try:
             result = GensimWord2Vec(source=JSONFile('../../../datasets/movies_info_reduced.json'),
                                     preprocessor=NLTK(),
-                                    field_name=["Title", "Year", "Genre"]).fit()
+                                    field_list=field_list).fit()
         except FileNotFoundError:
             result = GensimWord2Vec(source=JSONFile('datasets/movies_info_reduced.json'),
                                     preprocessor=NLTK(),
-                                    field_name=["Title", "Year", "Genre"]).fit()
+                                    field_list=field_list).fit()
