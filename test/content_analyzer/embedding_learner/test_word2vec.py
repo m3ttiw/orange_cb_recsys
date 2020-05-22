02b8a1429f582401456bcd5b729596311b09284c
Carlo Parisi <carlo.parisi01234@gmail.com>
2020-05-22 02:53:19 +0200

    fix bug

--- test/content_analyzer/embedding_learner/test_word2vec.py
+++ test/content_analyzer/embedding_learner/test_word2vec.py
@@ -8,7 +8,6 @@ import os
 
 class TestGensimWord2Vec(TestCase):
     def test_fit(self):
-        result = GensimWord2Vec(source=JSONFile(os.path.abspath(os.path.join("..", "..", "dataset",
-                                "movies_info_reduced.json"))),
+        result = GensimWord2Vec(source=JSONFile("dataset"),
                                 preprocessor=NLTK(),
                                 field_name="Genre").fit()
