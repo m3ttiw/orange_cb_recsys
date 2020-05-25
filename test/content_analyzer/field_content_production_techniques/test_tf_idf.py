8f5e440b987b5a6d3956ab57f6ee4393b9f83403
Roberto <roberto.barile98@gmail.com>
2020-05-25 17:52:06 +0200

    aggiunto test per tf-idf

---
+++ test/content_analyzer/field_content_production_techniques/test_tf_idf.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from orange_cb_recsys.content_analyzer.field_content_production_techniques.tf_idf import LuceneTfIdf
+from orange_cb_recsys.content_analyzer.information_processor.nlp import NLTK
+from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
+
+
+class TestLuceneTfIdf(TestCase):
+    def test_produce_content(self):
+        import lucene
+        lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+        file_path = '../../../datasets/movies_info_test.json'
+        try:
+            with open(file_path):
+                pass
+        except FileNotFoundError:
+            file_path = 'datasets/movies_info_test.json'
+
+        technique = LuceneTfIdf()
+        technique.append_field_need_refactor("Plot", str(1), [NLTK()])
+        technique.dataset_refactor(JSONFile(file_path), ["imdbID"])
+        features_bag_test = technique.produce_content("test", "tt0113497", "Plot", str(1))
