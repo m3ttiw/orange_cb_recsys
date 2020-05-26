9671fd94bd4856760ba4c6a3420d388b34156767
Roberto <roberto.barile98@gmail.com>
2020-05-26 23:21:13 +0200

    corretto setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='orange_cb_recsys',
-      version='1.2',
+      version='1.1.2.1',
       author='Roberto Barile, Francesco Benedetti, Carlo Parisi, Mattia Patruno',
       install_requires=[
-            'cython'
-            'wheel',
             'PyYAML==5.3.1',
             'numpy==1.18.4',
             'gensim==3.8.3',
@@ -20,7 +18,7 @@ setup(name='orange_cb_recsys',
             'wikipedia2vec==1.0.4'],
       description='Python Framework for Content-Based Recommeder Systems',
       url='https://github.com/m3ttiw/orange_cb_recsys',
-      download_url='https://github.com/m3ttiw/orange_cb_recsys/archive/1.0.tar.gz',
+      download_url='https://github.com/m3ttiw/orange_cb_recsys/archive/1.1.2.tar.gz',
       packages=['orange_cb_recsys', 'test', ],
       package_dir={'orange_cb_recsys': 'orange_cb_recsys', 'test': 'test', },
       )
