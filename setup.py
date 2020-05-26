6f7ec93a32d7839f7dd4d1e8971c323bbed4750f
Roberto <roberto.barile98@gmail.com>
2020-05-26 22:45:30 +0200

    corretta versione release

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(name='orange_cb_recsys',
-      version='1.0',
+      version='1.1',
+      install_requires=requirements,
       description='Python Framework for Content-Based Recommeder Systems',
-      url='https://github.com/m3ttiw/Framework_CBRS_py',
+      url='https://github.com/m3ttiw/orange_cb_recsys',
+      download_url='https://github.com/m3ttiw/orange_cb_recsys/archive/1.0.tar.gz',
       packages=['orange_cb_recsys', 'test', ],
       package_dir={'orange_cb_recsys': 'orange_cb_recsys', 'test': 'test', },
       )
-
-"""
-PER EVENTUALI DATASET
-setup(...,
-      packages=['mypkg'],
-      package_dir={'mypkg': 'orange_cb_recsys/mypkg'},
-      package_data={'mypkg': ['data/*.dat']},
-      )
-"""
