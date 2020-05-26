34fcac3bc4b55e9bea0ea56471149a56d3b4a5c5
Roberto <roberto.barile98@gmail.com>
2020-05-26 23:06:48 +0200

    rimosso manifest

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from distutils.core import setup
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='orange_cb_recsys',
-      version='1.1',
-      install_requires=requirements,
+      version='1.2',
+      author='Roberto Barile, Francesco Benedetti, Carlo Parisi, Mattia Patruno',
+      install_requires=[
+            'cython'
+            'wheel',
+            'PyYAML==5.3.1',
+            'numpy==1.18.4',
+            'gensim==3.8.3',
+            'nltk==3.5',
+            'babelpy==1.0.1',
+            'mysql==0.0.2',
+            'mysql-connector-python==8.0.20',
+            'wikipedia2vec==1.0.4'],
       description='Python Framework for Content-Based Recommeder Systems',
       url='https://github.com/m3ttiw/orange_cb_recsys',
       download_url='https://github.com/m3ttiw/orange_cb_recsys/archive/1.0.tar.gz',
