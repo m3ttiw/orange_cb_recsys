e8b81b33dfdffe5cb7a3b76d994ec9eeaf5fbb97
BlackienBad <carlo.parisi01234@gmail.com>
2020-05-31 00:02:12 +0200

    definito l'__init__ nella classe TextBlobSentimentalAnalysis

--- orange_cb_recsys/content_analyzer/field_content_production_techniques/sentimental_analysis.py
+++ orange_cb_recsys/content_analyzer/field_content_production_techniques/sentimental_analysis.py
@@ -2,14 +2,18 @@ from textblob import TextBlob
 
 from orange_cb_recsys.content_analyzer.field_content_production_techniques.field_content_production_technique import \
     SentimentalAnalysis
+from orange_cb_recsys.content_analyzer.raw_information_source import RawInformationSource
 
 
 class TextBlobSentimentalAnalysis(SentimentalAnalysis):
     """
     Interface for the textblob library that does sentimental analysis on text.
     """
-    def __init__(self):
+    def __init__(self, field_name: str,
+                 source: RawInformationSource):
         super().__init__()
+        self.__field_name = field_name
+        self.__source = source
 
     def calculate_score(self):
         pass
