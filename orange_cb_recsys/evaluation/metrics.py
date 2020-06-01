8a1544cf2129f07139ae1fcdb0d2d79cab5ee311
itsfrank98 <frybene@libero.it>
2020-06-01 12:25:39 +0200

    Implementate le metriche di valutazione per la rating prediction

--- orange_cb_recsys/evaluation/metrics.py
+++ orange_cb_recsys/evaluation/metrics.py
@@ -2,82 +2,173 @@ from typing import Dict
 import pandas as pd
 import numpy as np
 
-def find_matching_ratings(s1: pd.Series, s2: pd.Series):
-    for i in range (1,len(s1):
+
+def perform_precision(predictions: pd.Series, truth: pd.Series) -> float:
+    """
+    Calculates the precision of the recommendations provided
+
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth values
+
+    Returns:
+        float: precision
+    """
+    ncorrect = predictions.isin(truth).sum
+    return ncorrect/len(predictions)
+
+
+def perform_recall(predictions: pd.Series, truth: pd.Series) -> float:
+    """
+    Calculates the recall of the recommendations provided
+
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth values
+
+    Returns:
+        float: recall
+    """
+    ncorrect = predictions.isin(truth).sum
+    return ncorrect/len(truth)
+
+
+def perform_f1(precision, recall) -> float:
+    """
+    Calculates the f1-measure of the recommendations provided
+
+    Args:
+        precision (float): Precision of the recommendations
+        recall (float): Recall of the recommendations
+
+    Returns:
+        float: f1 measure
+    """
+    return 2 * ((precision * recall) / (precision + recall))
+
+
+def perform_dcg(scores: pd.Series):
+    """
+    Calculates the DCG of a given Series of scores
+
+    Args:
+        scores (pd.Series): Series of scores of which the function will find the DCG
+
+    Returns:
+        dcg (float): value of the DCG
+    """
+    dcg = 0
+    i = 0
+    for s in scores:
+        i += 1
+        dcg += s / np.log2(i)
+    return dcg
+
+
+def perform_ndcg(predictions: pd.Series, truth: pd.Series) -> float:
+    """
+    Calculates the NDCG, given by the ratio between the DCG of the recommendations provided and the
+    Ideal-DCG, represented by the DCG of the truth base
+
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth values
+
+    Returns:
+        float: value of the ndcg
+    """
+    dcg = perform_dcg(predictions)
+    ideal_dcg = perform_dcg(truth)
+    return dcg / ideal_dcg
 
 
 def perform_ranking_metrics(predictions: pd.Series, truth: pd.Series) -> Dict[str, object]:
-    def perform_precision():
-        """
-        Calculates the precision of the recommendations provided
-        """
-        ncorrect = predictions.isin(truth.index).sum()
-        return ncorrect/len(predictions)
+    """
+    Performs the metrics for evaluating the ranking phase and returns their values
 
-    def perform_recall():
-        """
-        Calculates the recall of the recommendations provided
-        """
-        ncorrect = predictions.isin(truth.index).sum()
-        return ncorrect/len(truth)
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth values
 
-    def perform_f1():
-        """
-        Calculates the f1-measure of the recommendations provided
-        """
-        prec = perform_precision()
-        rec = perform_recall()
-        return 2 * ((prec * rec)/ (prec + rec))
-
-    def __perform_dcg(scores: pd.Series):
-        """
-        Calculates the DCG of a given Series of scores
-        """
-        dcg, i = 0
-        for s in scores:
-            i += 1
-            dcg += s/np.log2(i)
-        return dcg
-
-    def perform_ndcg():
-        """
-        Calculates the NDCG, given by the ratio between the DCG of the recommendations provided and the
-        Ideal-DCG, represented by the DCG of the truth base
-        """
-        dcg = __perform_dcg(predictions)
-        ideal_dcg = __perform_dcg(truth)
-        return dcg / ideal_dcg
-
+    Returns:
+        results (Dict[str, object]): Python dictionary where the keys are the names of the metrics and the
+        values are the corresponding values
+    """
+    precision = perform_precision(predictions, truth)
+    recall = perform_recall(predictions, truth)
     results = {
-        "precision": perform_precision(),
-        "recall": perform_recall(),
-        "F1": perform_f1(),
-        "NDCG": perform_ndcg()
+        "precision": precision,
+        "recall": recall,
+        "F1": perform_f1(precision, recall),
+        "NDCG": perform_ndcg(predictions, truth)
     }
     return results
 
 
+def perform_gini_index():
+    pass
+
+
+def perform_pop_recs_correlation():
+    pass
+
+
 def perform_fairness_metrics() -> Dict[str, object]:
-    def perform_gini_index():
-        pass
 
-    def perform_pop_recs_correlation():
-        pass
-
-    results = {}
-    results["precision"] = perform_gini_index()
-    results["pop_recs_correlation"] = perform_pop_recs_correlation()
+    results = {
+        "precision": perform_gini_index(),
+        "pop_recs_correlation": perform_pop_recs_correlation()
+    }
     return results
 
 
+def perform_rmse(predictions: pd.Series, truth: pd.Series) -> float:
+    """
+    Compute the RMSE
+
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth rating values
+
+    Returns:
+        float: The Root Mean Squared Error
+    """
+    assert len(predictions) == len(truth), "The predictions series and the truth series must have the same size"
+    diff = predictions - truth
+    sq = np.square(diff)
+    return np.sqrt(np.mean(sq))
+
+
+def perform_mae(predictions: pd.Series, truth: pd.Series) -> float:
+    """
+    Compute the RMSE
+
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth rating values
+
+    Returns:
+        float: The Mean Average Error
+    """
+    assert len(predictions) == len(truth), "The predictions series and the truth series must have the same size"
+    abs_diff = (predictions - truth).apply(abs)
+    return np.mean(abs_diff)
+
+
 def perform_prediction_metrics(predictions: pd.Series, truth: pd.Series) -> Dict[str, object]:
-    def perform_RMSE():
-        pass
+    """
+    Performs the metrics for evaluating the rating prediction phase and returns their values
 
-    def perform_MAE():
-        pass
+    Args:
+        predictions (pd.Series): Series containing the predicted ratings
+        truth (pd.Series): Series containing the truth rating values
 
-    results = {}
-    results["RMSE"] = perform_RMSE()
-    results["MAE"] = perform_MAE()
+    Returns:
+        results (Dict[str, object]): Python dictionary where the keys are the names of the metrics and the
+        values are the corresponding values
+    """
+    results = {
+        "RMSE": perform_rmse(predictions, truth),
+        "MAE": perform_mae(predictions, truth)
+    }
     return results
