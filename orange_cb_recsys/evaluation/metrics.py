6c9f687d5c1eb677a7a16ae78a8445075ec677e0
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-01 11:09:32 +0200

    DCG e NDCG

--- orange_cb_recsys/evaluation/metrics.py
+++ orange_cb_recsys/evaluation/metrics.py
@@ -1,4 +1,4 @@
-from abc import ABC
+import numpy as np
 from typing import Dict
 import pandas as pd
 
@@ -11,14 +11,14 @@ def perform_ranking_metrics(predictions: pd.DataFrame,
         Returns the precision of the given ranking (predictions)
         based on the truth ranking
         """
-        return predictions.isin(truth.index).sum()/len(predictions)
+        return predictions.isin(truth.index).sum() / len(predictions)
 
     def perform_recall():
         """
         Returns the recall of the given ranking (predictions)
         based on the truth ranking
         """
-        return predictions.isin(truth.index).sum()/len(truth)
+        return predictions.isin(truth.index).sum() / len(truth)
 
     def perform_Fn(n: int = 1, precision: float = None, recall: float = None):
         """
@@ -27,10 +27,24 @@ def perform_ranking_metrics(predictions: pd.DataFrame,
         """
         p = precision if precision is not None else perform_precision()
         r = recall if recall is not None else perform_recall()
-        return (1 + n**2) * ((p * r) / (n**2 * p + r))
+        return (1 + n ** 2) * ((p * r) / (n ** 2 * p + r))
+
+    def perform_DCG(scores: pd.Series):
+        """
+        Returns the DCG measure of the given ranking (predictions)
+        based on the truth ranking
+        """
+        dcg = 0
+        for score, i in enumerate(scores):
+            dcg += score / np.log2(i)
+        return dcg
 
     def perform_NDCG():
-        return
+        """
+        Returns the NDCG measure of the given ranking (predictions)
+        based on the Ideal DCG of truth ranking
+        """
+        return perform_DCG(predictions.ix[:, 0]) / perform_DCG(truth.ix[:, 0])
 
     results = {
         "precision": perform_precision(),
@@ -58,7 +72,6 @@ def perform_fairness_metrics() -> Dict[str, object]:
     results["precision"] = perform_gini_index()
     results["pop_recs_correlation"] = perform_pop_recs_correlation()
 
-
     return results
 
 
@@ -73,6 +86,5 @@ def perform_prediction_metrics(predictions: pd.Series, truth: pd.Series) -> Dict
 
     results["RMSE"] = perform_RMSE()
     results["MAE"] = perform_MAE()
-
 
     return results
