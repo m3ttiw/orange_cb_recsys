aaf6cece45a949ff0a15ea7b6aec09c30131f712
Roberto <roberto.barile98@gmail.com>
2020-06-01 17:30:25 +0200

    modificato algoritmo ratings_based, cambiati content serializzati come esempio di risultato

--- orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
+++ orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
@@ -1,11 +1,12 @@
 from orange_cb_recsys.content_analyzer.content_representation.content import Content
 from orange_cb_recsys.recsys.score_prediction_algorithms.score_prediction_algorithm import RatingsSPA
 
+import pandas as pd
+
 
 class CentroidVector(RatingsSPA):
     def __init__(self, item_field: str):
         super().__init__(item_field)
 
-    def predict(self, user: Content, item: Content, ratings_field: str, items_directory: str):
-        ratings_values = user.get_field(ratings_field).get_representation(str(0))
-        # calcolo centroide
+    def predict(self, user: Content, item: Content, ratings: pd.DataFrame, items_directory: str):
+        pass
