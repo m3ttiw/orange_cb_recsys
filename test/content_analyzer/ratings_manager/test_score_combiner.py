abc39285bf1bf0a41ffb90af451eeb1aabc4c8d3
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-03 19:47:42 +0200

    test score combiner

--- test/content_analyzer/ratings_manager/test_score_combiner.py
+++ test/content_analyzer/ratings_manager/test_score_combiner.py
@@ -5,7 +5,8 @@ from orange_cb_recsys.content_analyzer.ratings_manager.score_combiner import Sco
 
 class TestScoreCombiner(TestCase):
     def test_combine(self):
-        test_list = [1.0, 0.0, 0.5]
-        self.assertAlmostEqual(ScoreCombiner("avg").combine(test_list), 0.5)
+        test_list = [1.0, 0.0, 0.5, 1.0]
+        self.assertAlmostEqual(ScoreCombiner("avg").combine(test_list), 0.625)
         self.assertAlmostEqual(ScoreCombiner("min").combine(test_list), 0.0)
         self.assertAlmostEqual(ScoreCombiner("max").combine(test_list), 1.0)
+        self.assertAlmostEqual(ScoreCombiner("mode").combine(test_list), 1.0)
