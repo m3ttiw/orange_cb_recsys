1de164bcc0b4dea578cc2f60371ca00cb4127339
Roberto <roberto.barile98@gmail.com>
2020-06-03 15:44:47 +0200

    riadattato test tf-idf

--- test/content_analyzer/field_content_production_techniques/test_tf_idf.py
+++ test/content_analyzer/field_content_production_techniques/test_tf_idf.py
@@ -4,15 +4,17 @@ from orange_cb_recsys.content_analyzer.field_content_production_techniques.tf_id
 from orange_cb_recsys.content_analyzer.information_processor.nlp import NLTK
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 
-
+import lucene
 class TestLuceneTfIdf(TestCase):
     def test_produce_content(self):
-        file_path = '../../../datasets/movies_info_test.json'
+        lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+        file_path = '../../../datasets/movies_info_reduced.json'
         try:
             with open(file_path):
                 pass
         except FileNotFoundError:
-            file_path = 'datasets/movies_info_test.json'
+            file_path = 'datasets/movies_info_reduced.json'
 
         technique = LuceneTfIdf()
         technique.append_field_need_refactor("Plot", str(1), [NLTK()])
@@ -20,5 +22,5 @@ class TestLuceneTfIdf(TestCase):
         features_bag_test = technique.produce_content("test", "tt0113497", "Plot", str(1))
         features = features_bag_test.get_value()
 
-        self.assertEqual(features['years'], 0)
+        self.assertEqual(features['years'], 0.6989700043360189)
 
