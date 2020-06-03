d46788f4911f26af99412289881eb6e349198582
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-03 17:02:36 +0200

    aggiornato il test della sentiment Analysis

--- test/content_analyzer/field_content_production_techniques/test_sentimental_analysis.py
+++ test/content_analyzer/field_content_production_techniques/test_sentimental_analysis.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from orange_cb_recsys.content_analyzer.ratings_manager.sentimental_analysis import \
     TextBlobSentimentalAnalysis
+from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 
 
 class TestTextBlobSentimentalAnalysis(TestCase):
@@ -13,6 +14,11 @@ class TestTextBlobSentimentalAnalysis(TestCase):
         except FileNotFoundError:
             file_path = 'datasets/test_sentiment_analysis.json'
 
-        confront_list = [1.0, 0.9, -0.6999999999999998, 0.8, 0.8125, 0.48333333333333334, 0.4166666666666667, 0.7666666666666666, 0.0, -0.15000000000000002, 0.0, 0.0, 0.0, 0.9, 0.39, 1.0, 0.16666666666666666, 0.3444444444444444, 0.3666666666666667, 0.525]
-        test_list: list = TextBlobSentimentalAnalysis().fit()
+        confront_list = [1.0, 0.9, -0.6999999999999998, 0.8, 0.8125, 0.48333333333333334, 0.4166666666666667,
+                         0.7666666666666666, 0.0, -0.15000000000000002, 0.0, 0.0, 0.0, 0.9, 0.39, 1.0,
+                         0.16666666666666666, 0.3444444444444444, 0.3666666666666667, 0.525]
+        test_list = []
+        source = JSONFile(file_path)
+        for test_field in source:
+            test_list.append(TextBlobSentimentalAnalysis().fit(field_data=test_field["rating"]))
         self.assertEqual(test_list, confront_list)
