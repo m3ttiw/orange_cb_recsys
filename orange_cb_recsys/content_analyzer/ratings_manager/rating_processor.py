dad780038f616755a4435beac7a78f79d511a59b
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-03 18:47:07 +0200

    test rating_processor.py

--- orange_cb_recsys/content_analyzer/ratings_manager/rating_processor.py
+++ orange_cb_recsys/content_analyzer/ratings_manager/rating_processor.py
@@ -27,9 +27,15 @@ class NumberNormalizer(RatingProcessor):
     Class that scale ratings in a numeric scale in the range [-1.0,1.0]
     """
     def __init__(self, min_: float, max_: float):
+        if min_ > max_:
+            max_, min_ = min_, max_
         self.__min = min_
         self.__max = max_
-        self.__scale_span = min_ - max_
+        self.__scale_span = abs(min_ - max_)
 
     def fit(self, field_data: float):
+        if field_data < self.__min:
+            return self.__min
+        if field_data > self.__max:
+            return self.__max
         return (float(field_data - self.__min) / float(self.__scale_span)) * 2 - 1
