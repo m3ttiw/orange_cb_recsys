746bdf7dfb504e7304310d87ce1b3001e4a91b20
Roberto <roberto.barile98@gmail.com>
2020-06-04 18:58:35 +0200

    aggiunte import facilitate per recsys e evaluation

---
+++ orange_cb_recsys/recsys/__init__.py
@@ -0,0 +1,3 @@
+from .ranking_algorithms import TopNRanking
+from .score_prediction_algorithms import CentroidVector
+from .config import RecSysConfig
---
+++ orange_cb_recsys/recsys/score_prediction_algorithms/__init__.py
@@ -0,0 +1,1 @@
+from .ratings_based import CentroidVector
