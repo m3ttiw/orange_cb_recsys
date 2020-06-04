9b9aebebfbccce14ef4a8be36dd222ca67f78ecc
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-04 11:58:45 +0200

    rimosse asserzioni e aggiunti warnings

--- orange_cb_recsys/evaluation/prediction_metrics.py
+++ orange_cb_recsys/evaluation/prediction_metrics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from warnings import warn
 
 
 def perform_rmse(predictions: pd.Series, truth: pd.Series) -> float:
@@ -13,7 +14,9 @@ def perform_rmse(predictions: pd.Series, truth: pd.Series) -> float:
     Returns:
         float: The Root Mean Squared Error
     """
-    assert len(predictions) == len(truth), "The predictions series and the truth series must have the same size"
+    if len(predictions) == len(truth):
+        warn("The predictions series and the truth series must have the same size")
+        return 0.0
     diff = predictions - truth
     sq = np.square(diff)
     return np.sqrt(np.mean(sq))
@@ -30,6 +33,8 @@ def perform_mae(predictions: pd.Series, truth: pd.Series) -> float:
     Returns:
         float: The Mean Average Error
     """
-    assert len(predictions) == len(truth), "The predictions series and the truth series must have the same size"
+    if len(predictions) == len(truth):
+        warn("The predictions series and the truth series must have the same size")
+        return 0.0
     abs_diff = (predictions - truth).apply(abs)
     return np.mean(abs_diff)
