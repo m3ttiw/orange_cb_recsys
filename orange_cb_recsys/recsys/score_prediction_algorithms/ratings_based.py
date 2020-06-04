9870de0aa1f18527c91507840df2691e3c036a16
BlackienBad <carlo.parisi01234@gmail.com>
2020-06-05 00:54:13 +0200

    Creato la struttura (da modificare) del metodo predict

--- orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
+++ orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+from sklearn.feature_extraction import DictVectorizer
+
 from orange_cb_recsys.content_analyzer.content_representation.content import Content
 from orange_cb_recsys.recsys.score_prediction_algorithms.score_prediction_algorithm import RatingsSPA
 
@@ -15,3 +19,19 @@ class CentroidVector(RatingsSPA):
 class ClassifierRecommender(RatingsSPA):
     def __init__(self, item_field: str, field_representation: str):
         super().__init__(item_field, field_representation)
+
+    def predict(self, user: Content, item: Content, ratings: pd.DataFrame, items_directory: str):
+        items = [filename for filename in os.listdir(items_directory)]
+
+        features_bag_list = []
+        rated_item_index_list = []
+        for item in items:
+            item_filename = items_directory + '/' + item
+            with open(item_filename, "rb") as content_file:
+                content = pickle.load(content_file)
+
+                features_bag_list.append(content.get_field("Plot").get_representation("1").get_value())
+        features_bag_list.append(content.get_field("Plot").get_representation("1").get_value())
+        v = DictVectorizer(sparse=False)
+
+        X_tmp = v.fit_transform(features_bag_list)
