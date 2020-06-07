c79ab3ae6f2bf6b563cb01a4619fa255693b561c
itsfrank98 <frybene@libero.it>
2020-06-07 21:23:55 +0200

    Modificato il modo in cui viene calcolato il centroide

--- orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
+++ orange_cb_recsys/recsys/score_prediction_algorithms/ratings_based.py
@@ -18,22 +18,26 @@ from orange_cb_recsys.utils.load_content import load_content_instance
 
 class CentroidVector(RatingsSPA):
     """
-    Class that implements a centroid-like recommender. It first gets the centroid of the items rated by the user
-    and then computes the similarity between the centroid and the item of which predict the score.
+    Class that implements a centroid-like recommender. It first gets the centroid of the items that the user liked.
+    Then computes the similarity between the centroid and the item of which predict the score.
     Args:
         item_field: Name of the field that contains the content to use
         field_representation: Id of the field_representation content of which compute the centroid and then the
         similarity (Similarity): Kind of similarity to use
+        threshold (int): Threshold for the ratings. If the rating is greater than the threshold, it will be considered
+        as positive
     """
-    def __init__(self, item_field: str, field_representation: str, similarity: Similarity):
+    def __init__(self, item_field: str, field_representation: str, similarity: Similarity, threshold: int = 0):
         super().__init__(item_field, field_representation)
         self.__similarity = similarity
+        self.__threshold = threshold
 
-    def __get_arrays(self, items_directory: str, rated_items: list) -> Dict[str, FieldRepresentation]:
+    def __get_arrays(self, items_directory: str, ratings: pd.DataFrame) -> Dict[str, FieldRepresentation]:
         """
         1) Iterates the files into items_directory
         2) For each file (that represents an item), checks if its id is present in rated_items. If false, skips
-        to the next file. If true, goes on item_field if exists
+        to the next file. Then checks on the rating. If is smaller than the threshold, skips. Otherwise goes on
+        item_field if exists
         3) Checks if the representation corresponding to field_representation exists
         4) Checks if the field representation is a document embedding (whose shape equals 1)
         5) If the previous checks went well, takes the value corresponding to the representation id and adds it to
@@ -50,14 +54,14 @@ class CentroidVector(RatingsSPA):
         Returns:
             arrays (dict<str, FieldRepresentation>): Dictionary whose keys are the id of the items and the values are
             the embedding arrays corresponding to the requested field
-
         """
         directory_item_list = [os.path.splitext(filename)[0] for filename in os.listdir(items_directory) if filename != 'search_index']
         arrays: dict = {}
         for item in directory_item_list:
             content = load_content_instance(items_directory, item)
             content_id = content.get_content_id()
-            if content_id in rated_items:
+            rating = ratings.loc[ratings['item_id'] == content_id].derived_score
+            if not rating.empty and rating.item() > self.__threshold:
                 if self.get_item_field() not in content.get_field_list():
                     raise ValueError("The field name specified could not be found!")
                 else:
@@ -72,28 +76,19 @@ class CentroidVector(RatingsSPA):
         return arrays
 
     @staticmethod
-    def __build_matrix(ratings: pd.DataFrame, arrays: dict) -> np.array:
+    def __build_matrix(arrays: dict) -> np.array:
         """
-        Builds a matrix containing the values of the arrays stored in arrays, weighted with the ratings stored in
-        ratings.
+        Builds a matrix containing the values of the arrays stored in arrays
 
         Args:
-            ratings (pd.DataFrame): DataFrame containing the ratings that the user gave to certain items
-            arrays (dict): Structure containing the values to weight
+            arrays (dict): Dictionary whose values mus be inserted in the matrix
 
         Returns:
              np.array: Matrix
         """
         matrix = []
-        if len(ratings) == 1:
-            item_id = ratings.iloc[0].item_id
-            rating = ratings.iloc[0].derived_score
-            matrix.append(rating * arrays[item_id])
-        else:
-            for i in range(len(ratings) - 1):
-                item_id = ratings.iloc[i].to_id
-                rating = ratings.iloc[i].score
-                matrix.append(rating * arrays[item_id])
+        for item_id in arrays.keys():
+            matrix.append(arrays[item_id])
         return np.array(matrix)
 
     @staticmethod
@@ -109,7 +104,7 @@ class CentroidVector(RatingsSPA):
         """
         return np.average(matrix, axis=0)
 
-    def predict(self, items: List[Content], ratings: pd.DataFrame, items_directory: str) -> Dict[str, float]:
+    def predict(self, items: List[Content], ratings: pd.DataFrame, items_directory: str) -> pd.DataFrame:
         """
         For each item:
         1) Takes the embedding arrays
@@ -122,26 +117,27 @@ class CentroidVector(RatingsSPA):
             items_directory (str): Name of the directory where the items are stored.
 
         Returns:
-             scores (Dict[str, float]): Dictionary whose keys are the ids of the items, and the values are the
-             similarities between the items and the centroid
+             scores (pd.DataFrame): DataFrame whose columns are the ids of the items, and the similarities between the
+              items and the centroid
         """
         try:
-            arrays = self.__get_arrays(items_directory, list(ratings.to_id))
-            matrix = self.__build_matrix(ratings, arrays)
+            arrays = self.__get_arrays(items_directory, ratings)
+            matrix = self.__build_matrix(arrays)
             centroid = self.__centroid(matrix)
+            columns = ["item_id", "rating"]
+            scores = pd.DataFrame(columns=columns)
+            for item in items:
+                item_id = item.get_content_id()
+                item_field_representation = item.get_field(self.get_item_field()).get_representation(
+                    self.get_field_representation()).get_value()
+                similarity = self.__similarity.perform(centroid, item_field_representation)
+                score = similarity * 2 - 1
+                scores = pd.concat([scores, pd.DataFrame.from_records([(item_id, score)], columns=columns)],
+                                   ignore_index=True)
+
+            return scores
         except ValueError as v:
             print(str(v))
-
-        columns = ["item_id", "rating"]
-        scores = pd.DataFrame(columns=columns)
-        for item in items:
-            item_id = item.get_content_id()
-            item_field_representation = item.get_field(self.get_item_field()).get_representation(self.get_field_representation()).get_value()
-            similarity = self.__similarity.perform(centroid, item_field_representation)
-            score = similarity * 2 - 1
-            scores = pd.concat([scores, pd.DataFrame.from_records([(item_id, score)], columns=columns)], ignore_index=True)
-
-        return scores
 
 
 class ClassifierRecommender(RatingsSPA):
