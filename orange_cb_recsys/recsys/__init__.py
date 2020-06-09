8f5a795582c7ac7846cdebee93966ce77bf688ce
Roberto <roberto.barile98@gmail.com>
2020-06-09 18:26:48 +0200

    corrette import semplificate nei file "__init__.py"

--- orange_cb_recsys/recsys/__init__.py
+++ orange_cb_recsys/recsys/__init__.py
@@ -1,1 +1,6 @@
 from .config import RecSysConfig
+from .recsys import RecSys
+from .score_prediction_algorithms.classifier import ClassifierRecommender
+from .ranking_algorithms.centroid_vector import CentroidVector
+from .ranking_algorithms.similarities import CosineSimilarity
+from .ranking_algorithms.index_query import IndexQuery
