1e1bbe0c4da18873ab6c0db56729ac8ebfa2d874
Roberto <roberto.barile98@gmail.com>
2020-06-09 11:19:41 +0200

    rifixato classificatore

--- orange_cb_recsys/recsys/score_prediction_algorithms/classifier.py
+++ orange_cb_recsys/recsys/score_prediction_algorithms/classifier.py
@@ -1,4 +1,3 @@
-import os
 from typing import List
 
 from sklearn.feature_extraction import DictVectorizer
@@ -9,7 +8,7 @@ from orange_cb_recsys.content_analyzer.content_representation.content import Con
 import pandas as pd
 
 from orange_cb_recsys.recsys.algorithm import ScorePredictionAlgorithm
-from orange_cb_recsys.utils.load_content import load_content_instance
+from orange_cb_recsys.utils.load_content import get_rated_items
 
 
 class ClassifierRecommender(ScorePredictionAlgorithm):
@@ -26,52 +25,42 @@ class ClassifierRecommender(ScorePredictionAlgorithm):
         """
         1) Goes into items_directory and for each item takes the values corresponding to the field_representation of
         the item_field. For example, if item_field == "Plot" and field_representation == "tf-idf", the function will
-        take the "tf-idf" representation of each  "Plot" field for every item, the tf-idf representation will
-        be parsed to dense arrays;
-        2) Takes a list of ratings that are in the dataframe (rated_item_index_list) and retrieves the corresponding
-        dense vectors, items with rating greater (lower) than treshold
-        will be used as positive(negative) examples;
-        3) Creates an object Classifier, uses the method fit and predicts the class of the item
+        take the "tf-idf" representation of each  "Plot" field for every rated item, the tf-idf representation of rated items
+        and items to classify will be parsed to dense arrays;
+        2) Define target features, items with rating greater (lower) than treshold will be used as positive(negative) examples;
+        3) Creates an object Classifier, uses the method fit and predicts the class of the new items
 
-                Args:
-                    items (List<Content>): Items for which the similarity will be computed
-                    ratings (pd.DataFrame): Ratings
-                    items_directory (str): Name of the directory where the items are stored.
+            Args:
+                items (List<Content>): Items for which the similarity will be computed
+                ratings (pd.DataFrame): Ratings
+                items_directory (str): Name of the directory where the items are stored.
 
-                Returns:
-                     The predicted classes, or the predict values.
-                """
+            Returns:
+                 The predicted classes, or the predict values.
+        """
 
         features_bag_list = []
-        rated_item_index_list = []
-        to_classify_item_dict = {item.get_content_id(): None for item in items}
 
-        directory_filename_list = [os.path.splitext(filename)[0] for filename in os.listdir(items_directory) if filename != 'search_index']
-        for i, item_filename in enumerate(directory_filename_list):
-            item = load_content_instance(items_directory, item_filename)
+        item_instances = get_rated_items(items_directory, ratings)
+        for i, item in enumerate(item_instances):
             features_bag_list.append(item.get_field(self.get_item_field()).get_representation(self.get_item_field_representation()).get_value())
 
-            if ratings['to_id'].str.contains(item.get_content_id()).any():
-                rated_item_index_list.append(i)
-
-            if item.get_content_id() in to_classify_item_dict.keys():
-                to_classify_item_dict[item.get_content_id()] = i
+        for item in items:
+            features_bag_list.append(item.get_field(self.get_item_field()).get_representation(self.get_item_field_representation()).get_value())
 
         v = DictVectorizer(sparse=False)
 
         score = [1 if rating > 0 else 0 for rating in list(ratings.score)]
 
         X_tmp = v.fit_transform(features_bag_list)
-        X = []
-        for item_id in rated_item_index_list:
-            X.append(X_tmp[item_id])
-
+        X = [X_tmp[i] for i in range(0, len(item_instances))]
+        print(len(X), len(score))
         clf = LogisticRegression()
         clf = clf.fit(X, score)
 
-        columns = ["item_id", "rating"]
+        columns = ["to_id", "rating"]
         score_frame = pd.DataFrame(columns=columns)
-        new_items = [X_tmp[to_classify_item_dict[item.get_content_id()]] for item in items]
+        new_items = [X_tmp[i] for i in range(len(X), len(features_bag_list))]
         scores = clf.predict_proba(new_items)
 
         for score, item in zip(scores, items):
--- orange_cb_recsys/utils/load_content.py
+++ orange_cb_recsys/utils/load_content.py
@@ -32,3 +32,24 @@ def get_unrated_items(items_directory, ratings):
         for item_id in item_to_predict_id_list]
 
     return item_to_predict_list
+
+
+def get_rated_items(items_directory, ratings):
+    directory_file_list = [os.path.splitext(filename)[0]
+                           for filename in os.listdir(items_directory)
+                           if filename != 'search_index']
+
+    # list of item id
+    directory_item_id_list = [
+        load_content_instance(items_directory, item_filename).get_content_id() for
+        item_filename in directory_file_list]
+
+    # list of id of item without rating
+    item_to_predict_id_list = [item_id for item_id in directory_item_id_list if
+                               ratings['to_id'].str.contains(item_id).any()]
+
+    item_to_predict_list = [
+        load_content_instance(items_directory, re.sub(r'[^\w\s]', '', item_id))
+        for item_id in item_to_predict_id_list]
+
+    return item_to_predict_list
