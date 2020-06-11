bdcd5b357ac3ae3cc3ef1f3de977415074a2e2d4
Roberto <roberto.barile98@gmail.com>
2020-06-11 15:30:57 +0200

    fix ordinamento centroide, rimossa colonna index inutile

--- orange_cb_recsys/recsys/ranking_algorithms/centroid_vector.py
+++ orange_cb_recsys/recsys/ranking_algorithms/centroid_vector.py
@@ -120,7 +120,7 @@ class CentroidVector(RankingAlgorithm):
                 scores = pd.concat([scores, pd.DataFrame.from_records([(item_id, similarity)], columns=columns)],
                                    ignore_index=True)
 
-            scores = scores.sort_values(['similarity'], ascending=False).reset_index()
+            scores = scores.sort_values(['similarity'], ascending=False).reset_index(drop=True)
             scores = scores[:recs_number]
 
             return scores
