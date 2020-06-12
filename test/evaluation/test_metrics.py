57354a4249603d67438985f5ba8098ea67dd0786
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-12 17:30:39 +0200

    fix ndcg calc

--- test/evaluation/test_metrics.py
+++ test/evaluation/test_metrics.py
@@ -71,11 +71,8 @@ class Test(TestCase):
         print(results['NDCG'])
 
     def test_NDCG(self):
-        self.assertEqual([], perform_NDCG(truth=pd.DataFrame({'to_id': ['aaa', 'bbb'], 'rating': [0.0, 1.0]}),
-                                          predictions=pd.DataFrame({'to_id': ['ccc', 'ddd'], 'rating': [0.0, 0.5]})),
-                         "null intersection")
-        perform_NDCG(truth=pd.DataFrame({'to_id': ['aaa', 'bbb', 'ccc'], 'rating': [0.1, 0.2, 0.3]}),
-                     predictions=pd.DataFrame({'to_id': ['ccc', 'ddd'], 'rating': [0.5, 0.6]}))
+        perform_NDCG(predictions=pd.DataFrame({'rating': [0.0, 0.5, 1.0, 0.3, -0.4]}))
+
 
     def test_perform_fairness_metrics(self):
         score_frame = pd.DataFrame.from_dict({'from_id': ["001", "001", "002", "002", "002", "003", "004", "004"],
