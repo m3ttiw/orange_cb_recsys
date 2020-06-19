369ae866bc7b399ddcab5c3540bb0ff7a1d18889
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-19 14:56:36 +0200

    LOD Properties retrieval con personalizzazione della modalità di ritrovamento delle informazioni: 'all', 'all_retrieved', 'only_retrieved_evaluated', 'original_retrieved'

--- orange_cb_recsys/content_analyzer/lod_properties_retrieval.py
+++ orange_cb_recsys/content_analyzer/lod_properties_retrieval.py
@@ -6,11 +6,31 @@ from orange_cb_recsys.utils.string_cleaner import clean_with_unders, clean_no_un
 
 
 class LODPropertiesRetrieval(ABC):
-    def __init__(self):
-        pass
+
+    def __init__(self, mode: str = 'only_retrieved_evaluated'):
+        self.__mode = self.__check_mode(mode)
+
+    @staticmethod
+    def __check_mode(mode):
+        modalities = [
+            'all',
+            'all_retrieved',
+            'only_retrieved_evaluated',
+            'original_retrieved',
+        ]
+        if mode in modalities:
+            return mode
+        else:
+            return 'all'
+
+    def set_mode(self, mode):
+        self.__mode = self.__check_mode(mode)
+
+    def get_mode(self):
+        return self.__mode
 
     @abstractmethod
-    def retrieve(self) -> Dict[str, str]:
+    def get_properties(self, raw_content: Dict[str, object]) -> Dict[str, str]:
         pass
 
 
@@ -20,8 +40,9 @@ class DBPediaMappingTechnique(LODPropertiesRetrieval):
         <property: property value URI>
     """
 
-    def __init__(self, entity_type: str, lang: str, label_field: str, additional_filters=None):
-        super().__init__()
+    def __init__(self, entity_type: str, lang: str, label_field: str, additional_filters=None,
+                 mode: str = 'only_retrieved_evaluated'):
+        super().__init__(mode)
 
         if additional_filters is None:
             additional_filters = {}
@@ -40,30 +61,33 @@ class DBPediaMappingTechnique(LODPropertiesRetrieval):
         self.__label_field = label_field
 
     def __check_has_label(self):
-        query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/>  "
-        query += "SELECT DISTINCT "
+        if len(self.__additional_filters) > 0:
+            query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/>  "
+            query += "SELECT DISTINCT "
 
-        query += ', '.join("?%s" % field_name.lower() for field_name in self.__additional_filters)
+            query += ', '.join("?%s" % field_name.lower() for field_name in self.__additional_filters)
 
-        query += " WHERE { "
+            query += " WHERE { "
 
-        query += '. '.join(
-            ["?uri dbo:" + property_name + ' ?' + field_name.lower() + "_tmp" for field_name, property_name in
-             self.__additional_filters.items()]) + '. '
+            query += '. '.join(
+                ["?uri dbo:" + property_name + ' ?' + field_name.lower() + "_tmp" for field_name, property_name in
+                 self.__additional_filters.items()]) + '. '
 
-        query += ' '.join(
-            ["OPTIONAL { ?" + field_name.lower() + "_tmp" + " rdfs:label" ' ?' + field_name.lower() + " }" for
-             field_name, property_name in
-             self.__additional_filters.items()])
+            query += ' '.join(
+                ["OPTIONAL { ?" + field_name.lower() + "_tmp" + " rdfs:label" ' ?' + field_name.lower() + " }" for
+                 field_name, property_name in
+                 self.__additional_filters.items()])
 
-        query += " } LIMIT 1 OFFSET 0"
+            query += " } LIMIT 1 OFFSET 0"
 
-        self.__sparql.setQuery(query)
-        results = self.__sparql.query().convert()
+            self.__sparql.setQuery(query)
+            results = self.__sparql.query().convert()
 
-        result = results["results"]["bindings"][0]
+            result = results["results"]["bindings"][0]
 
-        return result.keys()
+            return result.keys()
+        else:
+            return []
 
     def __mapping_query(self, raw_content):
         query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/>  "
@@ -75,7 +99,10 @@ class DBPediaMappingTechnique(LODPropertiesRetrieval):
         query += "?uri rdf:type dbo:%s . " % self.__entity_type
 
         # label matching
-        query += "?uri rdfs:label " + '?' + self.__label_field.lower() + '. '
+        query += "?uri rdfs:label " + '?' + self.__label_field.lower()
+
+        if (len(self.__additional_filters)) > 0:
+            query += '. '
 
         # filter fields assignments
         query += '. '.join(["?uri dbo:%s ?%s. " % (property_name, field_name.lower()) +
@@ -99,7 +126,8 @@ class DBPediaMappingTechnique(LODPropertiesRetrieval):
         query += ". FILTER langMatches(lang(?%s), \"%s\"). " % (self.__label_field.lower(), self.__lang)
 
         # label filter
-        query += "FILTER regex(?%s, \"%s\", \"i\"). " % (self.__label_field.lower(), clean_no_unders(raw_content[self.__label_field]))
+        query += "FILTER regex(?%s, \"%s\", \"i\"). " % (
+            self.__label_field.lower(), clean_no_unders(raw_content[self.__label_field]))
 
         query += " } "
 
@@ -153,31 +181,97 @@ class DBPediaMappingTechnique(LODPropertiesRetrieval):
 
         return result_dict
 
-    def produce_content(self, name, raw_content):
+    def __get_only_retrieved_evaluated(self, raw_content: Dict[str, object]) -> Dict[str, str]:
         new_property_labels = self.__get_properties_query()
-        original_property_labels = []
-
-        for key in raw_content.keys():
-            original_property_labels.append(key)
-
-        properties = {}
-
         try:
             uri = self.__mapping_query(raw_content)
             result_dict = self.__retrieve_property_values(uri, new_property_labels)
         except ValueError:
             result_dict = {}
+        return result_dict
 
+    def __get_all_properties_retrieved(self, raw_content: Dict[str, object]) -> Dict[str, str]:
+        new_property_labels = self.__get_properties_query()
+        result_dict = self.__get_only_retrieved_evaluated(raw_content)
+        properties = {}
         for property_label in new_property_labels:
             if property_label in result_dict.keys():
                 properties[property_label] = result_dict[property_label]
             else:
                 properties[property_label] = ""
+        return properties
+
+    def __get_original_retrieved(self, raw_content: Dict[str, object]) -> Dict[str, str]:
+        original_property_labels = []
+        original_properties = {}
+        for key in raw_content.keys():
+            original_property_labels.append(key)
+
+        retrieved_properties = self.__get_only_retrieved_evaluated(raw_content)
 
         for property_label in original_property_labels:
-            if property_label in raw_content.keys():
+            if property_label in retrieved_properties.keys():
+                original_properties[property_label] = retrieved_properties[property_label]
+            else:
+                original_properties[property_label] = ""
+
+        return original_properties
+
+    def __get_all_properties(self, raw_content: Dict[str, object]) -> Dict[str, str]:
+        all_prop_retrieved = self.__get_all_properties_retrieved(raw_content)
+        property_labels = self.__get_properties_query()
+        properties = {}
+        for key in raw_content.keys():
+            property_labels.append(key)
+
+        for property_label in property_labels:
+            if property_label in all_prop_retrieved.keys() and all_prop_retrieved[property_label] != '':
+                properties[property_label] = all_prop_retrieved[property_label]
+            elif property_label in raw_content.keys():
                 properties[property_label] = raw_content[property_label]
             else:
                 properties[property_label] = ""
+        return properties
 
-        print(properties)
+    def get_properties(self, raw_content: Dict[str, object]) -> Dict[str, str]:
+        if self.get_mode() == 'only_retrieved_evaluated':
+            return self.__get_only_retrieved_evaluated(raw_content)
+
+        if self.get_mode() == 'all_retrieved':
+            return self.__get_all_properties_retrieved(raw_content)
+
+        if self.get_mode() == 'original_retrieved':
+            return self.__get_original_retrieved(raw_content)
+
+        if self.get_mode() == 'all':
+            return self.__get_all_properties(raw_content)
+
+
+raw_content = {"Title": "Jumanji", "Year": "1995", "Rated": "PG", "Released": "15 Dec 1995", "Runtime": "104 min",
+               "Genre": "Adventure, Family, Fantasy", "Director": "Joe Johnston",
+               "Writer": "Jonathan Hensleigh (screenplay by), Greg Taylor (screenplay by), Jim Strain (screenplay by), Greg Taylor (screen story by), Jim Strain (screen story by), Chris Van Allsburg (screen story by), Chris Van Allsburg (based on the book by)",
+               "Actors": "Robin Williams, Jonathan Hyde, Kirsten Dunst, Bradley Pierce",
+               "Plot": "After being trapped in a jungle board game for 26 years, a Man-Child wins his release from the game. But, no sooner has he arrived that he is forced to play again, and this time sets the creatures of the jungle loose on the city. Now it is up to him to stop them.",
+               "Language": "English, French", "Country": "USA", "Awards": "4 wins & 9 nominations.",
+               "Poster": "https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+               "Ratings": [{"Source": "Internet Movie Database", "Value": "6.9/10"},
+                           {"Source": "Rotten Tomatoes", "Value": "53%"}, {"Source": "Metacritic", "Value": "39/100"}],
+               "Metascore": "39", "imdbRating": "6.9", "imdbVotes": "260,909", "imdbID": "tt0113497", "Type": "movie",
+               "DVD": "25 Jan 2000", "BoxOffice": "N/A", "Production": "Sony Pictures Home Entertainment",
+               "Website": "N/A", "Response": "True"}
+
+mapp = DBPediaMappingTechnique('Film', 'EN', 'Title')
+prop = mapp.get_properties(raw_content)
+print(prop)
+
+"""mapp.set_mode('all')
+prop = mapp.get_properties(raw_content)
+print(prop)
+
+mapp.set_mode('all_retrieved')
+prop = mapp.get_properties(raw_content)
+print(prop)
+
+mapp.set_mode('original_retrieved')
+prop = mapp.get_properties(raw_content)
+print(prop)"""
