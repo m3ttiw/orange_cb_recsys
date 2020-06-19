218f883186200c1e4d96adfd9b10c8b10ec367ed
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-19 19:19:24 +0200

    Test sum combining technique

--- test/content_analyzer/field_content_production_techniques/embedding_technique/test_combining_technique.py
+++ test/content_analyzer/field_content_production_techniques/embedding_technique/test_combining_technique.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import numpy as np
 
 from orange_cb_recsys.content_analyzer.field_content_production_techniques.embedding_technique.combining_technique import \
-    Centroid
+    Centroid, Sum
 
 
 class TestCentroid(TestCase):
@@ -18,5 +18,22 @@ class TestCentroid(TestCase):
 
         expected = np.ndarray(shape=(3, ))
         expected[:] = [2, 2, 2]
+
+        self.assertTrue((result == expected).all())
+
+
+class TestSum(TestCase):
+    def test_combine(self):
+        z = np.ndarray(shape=(3, 3))
+
+        z[0, :] = [1, 9, 1]
+        z[1, :] = [7, 2, 4]
+        z[2, :] = [3, 5, 3]
+
+        combiner = Sum()
+        result = combiner.combine(z)
+
+        expected = np.ndarray(shape=(3, ))
+        expected[:] = [11, 16, 8]
 
         self.assertTrue((result == expected).all())
