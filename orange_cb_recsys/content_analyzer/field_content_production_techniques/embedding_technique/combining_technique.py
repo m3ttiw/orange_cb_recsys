c6dded591cccb85c98aac72ec14ee78c02997e3c
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-19 19:18:34 +0200

    Implementata Sum combining_technique

--- orange_cb_recsys/content_analyzer/field_content_production_techniques/embedding_technique/combining_technique.py
+++ orange_cb_recsys/content_analyzer/field_content_production_techniques/embedding_technique/combining_technique.py
@@ -27,4 +27,28 @@ class Centroid(CombiningTechnique):
     def __repr__(self):
         return "< Centroid >"
 
+
+class Sum(CombiningTechnique):
+    """"
+    Class that implements the Abstract Class CombiningTechnique,
+    this class implements the sum vector of a matrix.
+    """
+    def combine(self, embedding_matrix: np.ndarray) -> np.ndarray:
+        """"
+        Calculates the centroid of the input matrix
+
+        Args:
+            embedding_matrix (np.ndarray): np bi-dimensional array whose centroid will be calculated
+
+        Returns:
+            np.ndarray: sum vector of the input matrix
+        """
+        return np.sum(embedding_matrix, axis=0)
+
+    def __str__(self):
+        return "Vector sum"
+
+    def __repr__(self):
+        return "< Vector sum >"
+
 # your combining technique
