d577819cfc2e9a06b0de1a263dc61b3aba063b8d
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-21 18:42:09 +0200

    Update grafo bipartito

--- orange_cb_recsys/recsys/graphs/bipartite_graphs.py
+++ orange_cb_recsys/recsys/graphs/bipartite_graphs.py
@@ -10,7 +10,7 @@ class NXBipartiteGraph(BipartiteGraph):
         super().__init__(source_frame)
 
     def create_graph(self):
-        self.__graph = nx.Graph()
+        self.__graph = nx.DiGraph()
 
     def add_node(self, node: object):
         self.__graph.add_node(node)
@@ -18,11 +18,17 @@ class NXBipartiteGraph(BipartiteGraph):
     def add_edge(self, from_node: object, to_node: object, weight: float, attr: List[object] = None):
         self.__graph.add_edge(from_node, to_node, weight=weight)
 
+    def get_edge(self, from_node: object, to_node: object):
+        try:
+            return self.__graph.get_edge_data(from_node, to_node)
+        except ValueError:
+            return None
+
     def get_adj(self, node: object) -> List[Tuple[object, object, float]]:
-        return self.__graph.adj
+        return self.__graph.neighbors(node)
 
     def get_predecessors(self, node: object) -> List[Tuple[object, object, float]]:
-        pass
+        return self.__graph.predecessors(node)
 
     def get_successors(self, node: object) -> List[Tuple[object, object, float]]:
-        pass
+        return self.__graph.successors(node)
