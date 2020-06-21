a9ac63cab98395f7c6058fd7cc17fb770585bf3a
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-06-21 19:13:57 +0200

    Creato grafo tripartito con lod properties

--- orange_cb_recsys/recsys/graphs/graph.py
+++ orange_cb_recsys/recsys/graphs/graph.py
@@ -1,24 +1,21 @@
+import lzma
+import pickle
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 import pandas as pd
+
+from orange_cb_recsys.content_analyzer.content_representation.content import Content
+from orange_cb_recsys.content_analyzer.ratings_manager.rating_processor import NumberNormalizer
+from orange_cb_recsys.content_analyzer.ratings_manager.sentiment_analysis import TextBlobSentimentAnalysis
 
 
 class Graph(ABC):
     """
     Abstract class that generalize the concept of a Graph
-    Attributes:
-        source_frame (pandas.DataFrame): must contains at least 'from', 'to', 'score' columns. The graph will be
-            generated from this DataFrame
     """
-    def __init__(self, source_frame: pd.DataFrame):
-        self.__graph = None
-        if self.__check_columns(source_frame):
-            self.create_graph()
-            for idx, row in source_frame.iterrows():
-                self.add_edge(row['from'], row['to'], self.normalize_score(row['score']))
-        else:
-            raise ValueError('The source frame must contains at least \'from\', \'to\', \'score\' columns')
+    def __init__(self):
+        pass
 
     @staticmethod
     def __check_columns(df: pd.DataFrame):
@@ -55,7 +52,8 @@ class Graph(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def add_edge(self, from_node: object, to_node: object, weight: float, attr: List[object] = None):
+    def add_edge(self, from_node: object, to_node: object, weight: float, label: str = 'weight',
+                 attr: List[object] = None):
         """ adds an edge, if the nodes are not in the graph, adds the nodes"""
         raise NotImplementedError
 
@@ -78,8 +76,21 @@ class Graph(ABC):
 
 
 class BipartiteGraph(Graph):
+    """
+    Abstract class that generalize the concept of a BipartiteGraph
+    Attributes:
+        source_frame (pandas.DataFrame): must contains at least 'from', 'to', 'score' columns. The graph will be
+            generated from this DataFrame
+    """
     def __init__(self, source_frame: pd.DataFrame):
-        super().__init__(source_frame)
+        super().__init__()
+        self.__graph = None
+        if self.__check_columns(source_frame):
+            self.create_graph()
+            for idx, row in source_frame.iterrows():
+                self.add_edge(row['from'], row['to'], self.normalize_score(row['score']))
+        else:
+            raise ValueError('The source frame must contains at least \'from\', \'to\', \'score\' columns')
 
     @abstractmethod
     def create_graph(self):
@@ -90,7 +101,8 @@ class BipartiteGraph(Graph):
         raise NotImplementedError
 
     @abstractmethod
-    def add_edge(self, from_node: object, to_node: object, weight: float, attr: List[object] = None):
+    def add_edge(self, from_node: object, to_node: object, weight: float, label: str = 'weight',
+                 attr: List[object] = None):
         """ adds an edge, if the nodes are not in the graph, adds the nodes"""
         raise NotImplementedError
 
@@ -114,15 +126,40 @@ class BipartiteGraph(Graph):
 
 class TripariteGraph(Graph):
     """ rating su più fields -> più archi (import di RatingsProcessor)"""
-    def __init__(self, source_frame: pd.DataFrame, contents_dir: str = None):
-        super().__init__(source_frame)
+    def __init__(self, source_frame: pd.DataFrame, contents_dir: str = None, **options):
+        self.__not_rated_value = 0.5
+        if 'not_rated_value' in options.keys():
+            self.__not_rated_value = self.normalize_score(options['not_rated_value'])
         self.__contents_dir = contents_dir
+        super().__init__()
+        self.__graph = None
+        if self.__check_columns(source_frame):
+            self.create_graph()
+            for idx, row in source_frame.iterrows():
+                self.add_edge(row['from'], row['to'], self.normalize_score(row['score']))
+                content = self.load_content(row['to'])
+                properties: dict = content.get_lod_properties()
+                for prop_key in properties.keys():
+                    preference = self.get_preference(prop_key, row)
+                    self.add_edge(row['to'], properties[prop_key], preference, prop_key)
 
-    def set_contents_dir(self, contents_dir: str):
-        self.__contents_dir = contents_dir
+        else:
+            raise ValueError('The source frame must contains at least \'from\', \'to\', \'score\' columns')
 
     def get_contents_dir(self) -> str:
         return self.__contents_dir
+
+    def get_preference(self, label: str, preferences_dict) -> float:
+        ls = '{}_score'.format(label)
+        if ls in preferences_dict.keys():
+            return preferences_dict[ls]
+        return self.__not_rated_value
+
+    @staticmethod
+    def load_content(file_name: str) -> Content:
+        with lzma.open('{}.xz'.format(file_name), 'r') as file:
+            content = pickle.load(file)
+        return content
 
     @abstractmethod
     def create_graph(self):
@@ -133,7 +170,8 @@ class TripariteGraph(Graph):
         raise NotImplementedError
 
     @abstractmethod
-    def add_edge(self, from_node: object, to_node: object, weight: float, attr: List[object] = None):
+    def add_edge(self, from_node: object, to_node: object, weight: float, label: str = 'weight',
+                 attr: List[object] = None):
         """ adds an edge, if the nodes are not in the graph, adds the nodes"""
         raise NotImplementedError
 
