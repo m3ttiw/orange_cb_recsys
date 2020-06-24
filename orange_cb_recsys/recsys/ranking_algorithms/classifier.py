17ca681b37c012bc2eada1d7ea1616507cb29ea9
Roberto <roberto.barile98@gmail.com>
2020-06-24 13:22:52 +0200

    fix

--- orange_cb_recsys/recsys/ranking_algorithms/classifier.py
+++ orange_cb_recsys/recsys/ranking_algorithms/classifier.py
@@ -24,9 +24,16 @@ from orange_cb_recsys.utils.load_content import get_rated_items, get_unrated_ite
 class ClassifierRecommender(RankingAlgorithm):
     """
        Class that implements a logistic regression classifier.
+
        Args:
            item_field (str): Name of the field that contains the content to use
            field_representation (str): Id of the field_representation content
+           classifier(str): classifier that will be used
+           can be one of the following values:
+           random_forest, svm, log_regr,
+           knn, decision_tree, gaussian_process
+           threshold: ratings bigger than threshold will be
+           considered as positive
        """
     def __init__(self, item_field: str, field_representation: str, classifier: str, threshold=-1):
         super().__init__(item_field, field_representation)
@@ -72,7 +79,7 @@ class ClassifierRecommender(RankingAlgorithm):
         labels = []
         for item in rated_items:
             if item is not None:
-                rated_features_bag_list.append(item.get_field(self.item_field).get_representation(self.item_field_representation()).value)
+                rated_features_bag_list.append(item.get_field(self.item_field).get_representation(self.item_field_representation).value)
                 labels.append(1 if float(ratings[ratings['to_id'] == item.content_id].score) >= threshold else 0)
 
         logger.info("Labeling examples")
