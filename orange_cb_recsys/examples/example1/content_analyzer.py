cbc0e6de498d7a523e35548e785dcecd11d67c2a
mattia <m3ttiw@gmail.com>
2020-09-17 18:00:08 +0200

    esempio 1

--- orange_cb_recsys/examples/example1/content_analyzer.py
+++ orange_cb_recsys/examples/example1/content_analyzer.py
@@ -3,15 +3,19 @@ from orange_cb_recsys.content_analyzer import ContentAnalyzerConfig, FieldRepres
 from orange_cb_recsys.content_analyzer.field_content_production_techniques import LuceneTfIdf
 from orange_cb_recsys.content_analyzer.field_content_production_techniques.tf_idf import SkLearnTfIdf
 from orange_cb_recsys.content_analyzer.information_processor import NLTK
-from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
+from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile, DATFile
 
 import lucene
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
-movies_filename = '../../../datasets/movies_info_reduced.json'
+movies_filename = '../../../datasets/examples/movies_info.json'
 
 movies_output_dir = '../../../contents/examples/ex_1/movies_'
+
+users_filename = '../../../datasets/examples/users_70.dat'
+
+users_output_dir = '../../../contents/examples/ex_1/users_'
 
 movies_ca_config = ContentAnalyzerConfig(
     content_type='Item',
@@ -60,6 +64,17 @@ movies_ca_config.append_field_config(
 )
 
 ContentAnalyzer(config=movies_ca_config).fit()
+
+
+users_ca_config = ContentAnalyzerConfig(
+    content_type='Item',
+    source=DATFile(users_filename),
+    id_field_name_list=['0'],
+    output_directory=users_output_dir,
+
+)
+
+ContentAnalyzer(config=users_ca_config).fit()
 
 
 # esempio di lettura del contenuto
--- orange_cb_recsys/examples/example1/recsys.py
+++ orange_cb_recsys/examples/example1/recsys.py
@@ -1,18 +1,22 @@
 from orange_cb_recsys.content_analyzer.ratings_manager import RatingsImporter
 from orange_cb_recsys.content_analyzer.ratings_manager.rating_processor import NumberNormalizer
 from orange_cb_recsys.content_analyzer.ratings_manager.ratings_importer import RatingsFieldConfig
-from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
-from orange_cb_recsys.evaluation import RankingAlgEvalModel, KFoldPartitioning, NDCG, ReportEvalModel, FNMeasure
+from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile, CSVFile
+from orange_cb_recsys.evaluation import RankingAlgEvalModel, KFoldPartitioning, NDCG, ReportEvalModel, FNMeasure, \
+    Correlation, Precision, Recall
+from orange_cb_recsys.evaluation.prediction_metrics import MAE
 from orange_cb_recsys.recsys import CentroidVector, CosineSimilarity, RecSysConfig, RecSys
 
-ratings_filename = '../../../datasets/ratings_example.json'
+ratings_filename = '../../../datasets/examples/new_ratings.csv'
 
-ca_dir = '../../../contents/examples/ex_1/movies_1600270629.4642215'
+items_ca_dir = '../../../contents/examples/ex_1/movies_1600355466.500496'
+
+users_ca_dir = '../../../contents/examples/ex_1/users_1600355755.1935306'
 
 ratings_importer = RatingsImporter(
-    source=JSONFile(ratings_filename),
+    source=CSVFile(ratings_filename),
     rating_configs=[RatingsFieldConfig(
-        field_name='stars',
+        field_name='points',
         processor=NumberNormalizer(min_=1, max_=5))],
     from_field_name='user_id',
     to_field_name='item_id',
@@ -32,8 +36,8 @@ centroid_config = CentroidVector(
 
 
 centroid_recsys_config = RecSysConfig(
-    users_directory=ca_dir,
-    items_directory=ca_dir,
+    users_directory=users_ca_dir,
+    items_directory=items_ca_dir,
     ranking_algorithm=centroid_config,
     rating_frame=ratings_frame
 )
@@ -44,7 +48,7 @@ centroid_recommender = RecSys(
 )
 
 rank = centroid_recommender.fit_ranking(
-    user_id='01',
+    user_id='1',
     recs_number=5
 )
 
@@ -55,7 +59,7 @@ print(rank)
 evaluation_centroid = RankingAlgEvalModel(
     config=centroid_recsys_config,
     partitioning=KFoldPartitioning(n_splits=4),
-    metric_list=[NDCG(), FNMeasure(n=2)]
+    metric_list=[FNMeasure(n=2), Precision(), Recall()]
 )
 
 results = evaluation_centroid.fit()
